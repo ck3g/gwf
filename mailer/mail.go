4c8d306f41e9e3a9cb3190e085c5d3b7ac06ffca
Vitali Tatarintev <Kalastiuz@gmail.com>
2022-01-09 11:16:27 +0100

    Describe Mailer package

---
+++ mailer/mail.go
@@ -0,0 +1,55 @@
+package mailer
+
+type Mail struct {
+	Domain      string
+	Templates   string
+	Host        string
+	Port        string
+	Username    string
+	Password    string
+	Encryption  string
+	FromAddress string
+	FromName    string
+	Jobs        chan Message
+	Results     chan Result
+	API         string
+	APIKey      string
+	APIUrl      string
+}
+
+type Message struct {
+	From        string
+	FromName    string
+	To          string
+	Subject     string
+	Template    string
+	Attachments []string
+	Data        interface{}
+}
+
+type Result struct {
+	Success bool
+	Error   error
+}
+
+func (m *Mail) ListenForMail() {
+	for {
+		msg := <-m.Jobs
+		err := m.Send(msg)
+		if err != nil {
+			m.Results <- Result{false, err}
+		} else {
+			m.Results <- Result{true, nil}
+		}
+	}
+}
+
+func (m *Mail) Send(msg Message) error {
+	// TODO: are we using an API or SMTP
+
+	return m.SendSMTPMessage(msg)
+}
+
+func (m *Mail) SendSMTPMessage(msg Message) error {
+	return nil
+}
