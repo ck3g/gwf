7c39a308fed588dcf1d456fde0019fa44decc1bc
Vitali Tatarintev <Kalastiuz@gmail.com>
2022-01-15 09:55:00 +0100

    Test mailer functions

--- mailer/mail_test.go
+++ mailer/mail_test.go
@@ -1,19 +1,103 @@
 package mailer
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
+
+var msg = Message{
+	From:        "me@here.com",
+	FromName:    "Joe",
+	To:          "you@there.com",
+	Subject:     "Test",
+	Template:    "test",
+	Attachments: []string{"./testdata/mail/test.html.tmpl"},
+}
 
 func TestMail_SendSMTPMessage(t *testing.T) {
+	err := mailer.SendSMTPMessage(msg)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestMail_SendUsingChan(t *testing.T) {
+	mailer.Jobs <- msg
+	res := <-mailer.Results
+	if res.Error != nil {
+		t.Error(errors.New("failed to send over channel"))
+	}
+
+	msg.To = "no_an_email_address"
+	mailer.Jobs <- msg
+	res = <-mailer.Results
+	if res.Error == nil {
+		t.Error(errors.New("no error received with invalid To address"))
+	}
+
+	msg.To = "you@there.com"
+}
+
+func TestMail_SendUsingAPI(t *testing.T) {
 	msg := Message{
-		From:        "me@here.com",
-		FromName:    "Joe",
 		To:          "you@there.com",
 		Subject:     "Test",
 		Template:    "test",
 		Attachments: []string{"./testdata/mail/test.html.tmpl"},
 	}
 
-	err := mailer.SendSMTPMessage(msg)
+	mailer.API = "unknown"
+	mailer.APIKey = "abc123"
+	mailer.APIUrl = "https://www.fake.com"
+
+	err := mailer.SendUsingAPI(msg, "unknown")
+	if err == nil {
+		t.Error(err)
+	}
+	mailer.API = ""
+	mailer.APIKey = ""
+	mailer.APIUrl = ""
+}
+
+func TestMail_buildHTMLMessage(t *testing.T) {
+	_, err := mailer.buildHTMLMessage(msg)
 	if err != nil {
 		t.Error(err)
 	}
+}
+
+func TestMail_buildPlainTextMessage(t *testing.T) {
+	_, err := mailer.buildPlainTextMessage(msg)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestMail_send(t *testing.T) {
+	err := mailer.Send(msg)
+	if err != nil {
+		t.Error(err)
+	}
+
+	mailer.API = "unknown"
+	mailer.APIKey = "abc123"
+	mailer.APIUrl = "https://www.fake.com"
+
+	err = mailer.Send(msg)
+	if err == nil {
+		t.Error(err)
+	}
+	mailer.API = ""
+	mailer.APIKey = ""
+	mailer.APIUrl = ""
+}
+
+func TestMail_ChooseAPI(t *testing.T) {
+	mailer.API = "unknown"
+	err := mailer.ChooseAPI(msg)
+	if err == nil {
+		t.Error(err)
+	}
+
+	mailer.API = ""
 }
