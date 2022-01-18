b6f29ae05e740ee5a7ce3d5326c22770f946ea4c
Vitali Tatarintev <Kalastiuz@gmail.com>
2022-01-18 08:34:24 +0100

    Describe a roadmap of 'make new app_name' CLI command

---
+++ cmd/cli/new.go
@@ -0,0 +1,23 @@
+package main
+
+import "strings"
+
+func doNew(appName string) {
+	appName = strings.ToLower(appName)
+
+	// sanitize the application name (convert url to single word)
+
+	// git clone the skeleton application
+
+	// remove .git directory
+
+	// create a ready to go .env file
+
+	// create a Makefile
+
+	// update the go.mod file
+
+	// update the existing .go files with correct name and imports
+
+	// run go mod tidy in the project directory
+}
