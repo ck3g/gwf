0c3e6391c8ba51d376c1eeff6ae2ff1ab7769086
Vitali Tatarintev <Kalastiuz@gmail.com>
2022-01-18 09:27:16 +0100

    Generate a proper .env file

--- cmd/cli/new.go
+++ cmd/cli/new.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -33,8 +34,26 @@ func doNew(appName string) {
 	}
 
 	// remove .git directory
+	err = os.RemoveAll(fmt.Sprintf("./%s/.git", appName))
+	if err != nil {
+		exitGracefully(err)
+	}
 
 	// create a ready to go .env file
+	color.Yellow("\tCreating .env file...")
+	data, err := templateFS.ReadFile("templates/env.txt")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	env := string(data)
+	env = strings.ReplaceAll(env, "${APP_NAME}", appName)
+	env = strings.ReplaceAll(env, "${KEY}", g.RandomString(32))
+
+	err = copyDataToFile([]byte(env), fmt.Sprintf("./%s/.env", appName))
+	if err != nil {
+		exitGracefully(err)
+	}
 
 	// create a Makefile
 
